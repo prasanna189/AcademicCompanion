e81b63dcebcc3ecf00d159972eff0211142c8326
Prasanna Kumar <spkforwork@gmail.com>
2017-04-04 22:30:23 +0530

    modified mainactivity.java so that inro screens and main home screen are separated. still not running. some changes required to get full functionality.

--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -1,101 +1,32 @@
 package com.example.adavi.academiccompanion;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 //just testing
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private TextView[] dots;
-    Button next,skip;
-    private LinearLayout dotsLayout;
-    private ViewPager viewPager;
-    private ViewPageAdapter viewPageAdapter;
-    private IntroActivity one;
+    SharedPreferences prefs = null;
 
-    private int[] layouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        one = new IntroActivity(this);
-        if(!one.Check())
-        {
-            one.setFirst(false);
-            Intent i = new Intent(MainActivity.this,MainActivity2.class);
-            startActivity(i);
-            finish();
-        }
-
-        if(Build.VERSION.SDK_INT>=21)
-        {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager)findViewById(R.id.view_pager);
-        dotsLayout = (LinearLayout)findViewById(R.id.layoutDots);
-        skip = (Button)findViewById(R.id.btn2);
-        next = (Button)findViewById(R.id.btn1);
-        layouts= new int[]{R.layout.activity_screen1,R.layout.activity_screen2,R.layout.activity_screen3};
-        addBottomDots(0);
-        changeStatusBarColor();
-        viewPageAdapter = new ViewPageAdapter();
-        viewPager.setAdapter(viewPageAdapter);
-        viewPager.addOnPageChangeListener(viewListener);
-        skip.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int current = getItem(+1);
-                if(current<layouts.length)
-                {
-                    viewPager.setCurrentItem(current);
-                }
-                else
-                {
-                    Intent i = new Intent(MainActivity.this,MainActivity2.class);
-                    startActivity(i);
-                    finish();
-                }
-            }
-        });
-
+        prefs = getSharedPreferences("com.example.adavi.academiccompanion", MODE_PRIVATE);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -108,6 +39,8 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -118,98 +51,19 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    private void addBottomDots(int position)
-    {
-        dots = new TextView[layouts.length];
-        int [] colorActive= getResources().getIntArray(R.array.dot_active);
-        int [] colorInactive = getResources().getIntArray(R.array.dot_inactive);
-        dotsLayout.removeAllViews();
-        for(int i=0;i<dots.length;i++)
-        {
-            dots[i] = new TextView(this);
-            dots[i].setText(Html.fromHtml("&#8226;"));
-            dots[i].setTextSize(35);
-            dots[i].setTextColor(colorInactive[position]);
-            dotsLayout.addView(dots[i]);
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-        }
-        if(dots.length>0)
-        {
-            dots[position].setTextColor(colorActive[position]);
+        if (prefs.getBoolean("firstrun", true)) {
+            // Do first run stuff here then set 'firstrun' as false
+            // using the following line to edit/commit prefs
+            Intent intent = new Intent(this, IntroSlidesActivity.class);
+            startActivity(intent);
+            prefs.edit().putBoolean("firstrun", false).commit();
         }
     }
 
-    private int getItem(int i){
-        return viewPager.getCurrentItem()+1;
-    }
- ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener(){
-     @Override
-     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-     }
-
-     @Override
-     public void onPageSelected(int position) {
-        addBottomDots(position);
-         if(position==layouts.length-1)
-         {
-             next.setText("PROCEED");
-             skip.setVisibility(View.GONE);
-         }
-         else
-         {
-             next.setText("NEXT");
-             skip.setVisibility(View.VISIBLE);
-         }
-     }
-
-     @Override
-     public void onPageScrollStateChanged(int state) {
-
-     }
- };
-    private void changeStatusBarColor()
-    {
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
-        {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-    }
-
-    public class ViewPageAdapter extends PagerAdapter
-    {
-        private LayoutInflater lif;
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            lif=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View v=lif.inflate(layouts[position],container,false);
-            container.addView(v);
-            return v;
-        }
-
-        @Override
-        public int getCount()
-        {
-            return layouts.length;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-
-            return view==object;
-
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View v =(View)object;
-            container.removeView(v);
-        }
-
-    }
 
     @Override
     public void onBackPressed() {
