8f8b9a10f8ca254db17ae2ec58881d1e25de43f8
Prasanna Kumar <spkforwork@gmail.com>
2017-04-04 18:20:51 +0530

    removed errors and testing.

--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -1,25 +1,21 @@
 package com.example.adavi.academiccompanion;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 
-//just testing
+//accomplishment report - https://1drv.ms/w/s!AvhBWlceN1rohaBB6AmTC1u7Pc_AdQ
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -42,8 +38,8 @@ public class MainActivity extends AppCompatActivity
             startActivity(i);
             finish();
         }
-        setContentView(R.layout.activity_main);
-        layouts= new int[]{R.layout.activity_screen1,R.layout.activity_screen2,R.layout.activity_screen3};
+        setContentView(R.layout.activity_screen1);
+//        layouts= new int[]{R.layout.activity_screen1,R.layout.activity_screen2,R.layout.activity_screen3};
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -67,26 +63,26 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
-    public class ViewPageAdapter extends PagerAdapter
+//    public class ViewPageAdapter extends PagerAdapter
     {
-        private LayoutInflater lif;
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            lif=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View v=LayoutInflater.inflate();
-            return super.instantiateItem(container, position);
-        }
-
-        @Override
-        public int getCount() {
-            return 0;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return false;
-        }
+//        private LayoutInflater lif;
+//
+//        @Override
+//        public Object instantiateItem(ViewGroup container, int position) {
+//            lif=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            View v=LayoutInflater.inflate();
+//            return super.instantiateItem(container, position);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public boolean isViewFromObject(View view, Object object) {
+//            return false;
+//        }
     }
 
     @Override
