369a4811c42f997ac073727dd91560103755ca4b
Prasanna Kumar <spkforwork@gmail.com>
2017-04-04 17:38:21 +0530

    removed errors and testing.

--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -19,7 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-//just making change
+//just testing
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
