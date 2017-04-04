bbf85fee285dda973b661d979aca88b680d39510
asc96 <akhilsaichandra17@gmail.com>
2017-04-04 19:19:42 +0530

    modified view page adapter in mainactivity.java

--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.adavi.academiccompanion;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,20 +15,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 //just testing
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
-    private ViewPager viewpager;
-
+    private TextView[] dots;
+    Button next,skip;
+    private LinearLayout dotsLayout;
+    private ViewPager viewPager;
+    private ViewPageAdapter viewPageAdapter;
     private IntroActivity one;
 
     private int[] layouts;
@@ -42,8 +52,49 @@ public class MainActivity extends AppCompatActivity
             startActivity(i);
             finish();
         }
+
+        if(Build.VERSION.SDK_INT>=21)
+        {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout)findViewById(R.id.layoutDots);
+        skip = (Button)findViewById(R.id.btn2);
+        next = (Button)findViewById(R.id.btn1);
         layouts= new int[]{R.layout.activity_screen1,R.layout.activity_screen2,R.layout.activity_screen3};
+        addBottomDots(0);
+        changeStatusBarColor();
+        viewPageAdapter = new ViewPageAdapter();
+        viewPager.setAdapter(viewPageAdapter);
+        viewPager.addOnPageChangeListener(viewListener);
+        skip.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if(current<layouts.length)
+                {
+                    viewPager.setCurrentItem(current);
+                }
+                else
+                {
+                    Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -67,6 +118,66 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+    private void addBottomDots(int position)
+    {
+        dots = new TextView[layouts.length];
+        int [] colorActive= getResources().getIntArray(R.array.dot_active);
+        int [] colorInactive = getResources().getIntArray(R.array.dot_inactive);
+        dotsLayout.removeAllViews();
+        for(int i=0;i<dots.length;i++)
+        {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorInactive[position]);
+            dotsLayout.addView(dots[i]);
+
+        }
+        if(dots.length>0)
+        {
+            dots[position].setTextColor(colorActive[position]);
+        }
+    }
+
+    private int getItem(int i){
+        return viewPager.getCurrentItem()+1;
+    }
+ ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener(){
+     @Override
+     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+     }
+
+     @Override
+     public void onPageSelected(int position) {
+        addBottomDots(position);
+         if(position==layouts.length-1)
+         {
+             next.setText("PROCEED");
+             skip.setVisibility(View.GONE);
+         }
+         else
+         {
+             next.setText("NEXT");
+             skip.setVisibility(View.VISIBLE);
+         }
+     }
+
+     @Override
+     public void onPageScrollStateChanged(int state) {
+
+     }
+ };
+    private void changeStatusBarColor()
+    {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP)
+        {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
     public class ViewPageAdapter extends PagerAdapter
     {
         private LayoutInflater lif;
@@ -74,19 +185,30 @@ public class MainActivity extends AppCompatActivity
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             lif=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View v=LayoutInflater.inflate();
-            return super.instantiateItem(container, position);
+            View v=lif.inflate(layouts[position],container,false);
+            container.addView(v);
+            return v;
         }
 
         @Override
-        public int getCount() {
-            return 0;
+        public int getCount()
+        {
+            return layouts.length;
         }
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return false;
+
+            return view==object;
+
         }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View v =(View)object;
+            container.removeView(v);
+        }
+
     }
 
     @Override
@@ -97,28 +219,6 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
