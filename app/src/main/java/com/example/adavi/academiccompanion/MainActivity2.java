3bae7a10366b3dc62cb5d0dedc430c76d5dcd891
Prasanna Kumar <spkforwork@gmail.com>
2017-04-04 16:35:18 +0530

    added missing files

---
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.example.adavi.academiccompanion;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
