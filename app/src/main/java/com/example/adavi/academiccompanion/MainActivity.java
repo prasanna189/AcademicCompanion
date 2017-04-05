4bf23d7d34823180509e660fb5eea82f4d7cf1e4
Prasanna Kumar <spkforwork@gmail.com>
2017-04-05 15:40:16 +0530

    added some icons.

--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
