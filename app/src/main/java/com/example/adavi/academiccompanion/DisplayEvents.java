7c1bd750108e259799f2c14ad6af729f7c17c7b0
dvarun97 <donivarun97@gmail.com>
2017-04-07 15:51:58 +0530

    updateCurrentSem

---
+++ app/src/main/java/com/example/adavi/academiccompanion/DisplayEvents.java
@@ -0,0 +1,100 @@
+package com.example.adavi.academiccompanion;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_events);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void displayEvents(String sname, String tname, String temail) {
+
+        //layout to which children are added
+        RelativeLayout subjectLL = (RelativeLayout) findViewById(R.id.subjects_ll_button);
+
+
+        //child layouts
+        Button rowButton = new Button(this);
+        TextView tv = new TextView(this);
+        LinearLayout ll = new LinearLayout(this);
+
+
+        //layout params for each view
+
+        LinearLayout.LayoutParams ll_params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        ll_params.setMargins(24, 24, 24, 24);
+
+
+        LinearLayout.LayoutParams rb_params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                1.0f
+        );
+
+//        rb_params.setMargins(8, 8, 8, 8);
+
+        LinearLayout.LayoutParams tv_params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                4.0f
+        );
+
+        tv_params.setMargins(24, 8, 8, 8);
+
+        tv.setLayoutParams(tv_params);
+        ll.setLayoutParams(ll_params);
+        rowButton.setLayoutParams(rb_params);
+
+
+        rowButton.setText(sname);
+        rowButton.setTextSize(20);
+        rowButton.setBackgroundColor(Color.rgb(224, 242, 241));
+
+        tv.setText(tname);
+        tv.setTextSize(12);
+
+
+        ll.setBackgroundColor(Color.rgb(224, 242, 241));
+        ll.addView(rowButton);
+        ll.addView(tv);
+
+        subjectLL.addView(ll);
+    }
+
+
+    public void addEvents(View view)
+    {
+        Intent intent = new Intent(this, AddNewEvent.class);
+        startActivity(intent);
+    }
+
+}
