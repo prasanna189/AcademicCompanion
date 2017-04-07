692eb9ac4c01b9983b645b05831af6114bbd0377
Prasanna Kumar <spkforwork@gmail.com>
2017-04-07 15:54:27 +0530

    added database manager.

--- app/src/main/java/com/example/adavi/academiccompanion/IntroUserInputActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/IntroUserInputActivity.java
@@ -1,7 +1,6 @@
 package com.example.adavi.academiccompanion;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,20 +9,23 @@ import android.widget.EditText;
 public class IntroUserInputActivity extends AppCompatActivity {
 
 
+    DatabaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_user_input);
+        myDB = new DatabaseHelper(this);
+
     }
 
     void submit(View view) {
         boolean flag = true;
-
-
-
         EditText nameId = (EditText) findViewById(R.id.name_id);
         EditText semId = (EditText) findViewById(R.id.sem_id);
         EditText emailId = (EditText) findViewById(R.id.email_id);
+
+        /*
         if (nameId.getText().toString().length() == 0) {
             nameId.setError("Please Enter Your Name");
             flag = false;
@@ -36,14 +38,24 @@ public class IntroUserInputActivity extends AppCompatActivity {
             emailId.setError("Please Enter Your Email ID");
             flag = false;
         }
+        */
+
         if (flag)
         {
+            boolean a,b,c;
 
-            //setting firsttimelaunch as false, so from next time directly home screen is opened.
+            myDB.setCurrentSem( semId.getText().toString() );
+
+            a = myDB.insertDataUserDetails(nameId.getText().toString(), emailId.getText().toString(), "", semId.getText().toString());
+            c = myDB.insertDataSemester( semId.getText().toString() );
+
             com.example.adavi.academiccompanion.WelcomeActivity.prefManager.setFirstTimeLaunch(false);
             Intent intent = new Intent(IntroUserInputActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
+
+            //setting firsttimelaunch as false, so from next time directly home screen is opened.
+
         }
     }
 }
--- app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.View;
 //just testing
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
 
 
     @Override
@@ -82,6 +82,22 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+//    Button button = (Button)findViewById(R.id.dbmanager_id);
+//
+//    button.setOnClickListener(new OnClickListener() {
+//        public void onClick(View v) {
+//
+//            Intent dbmanager = new Intent(getActivity(),AndroidDatabaseManager.class);
+//            startActivity(dbmanager);
+//        }
+//    });
+
+    public void databasemanager(View view)
+    {
+        Intent dbmanager = new Intent(this,AndroidDatabaseManager.class);
+        startActivity(dbmanager);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
