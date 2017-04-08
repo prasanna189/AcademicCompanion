1e3315bc54df8e56b7501bc791aba74232aa7eb2
Prasanna Kumar <spkforwork@gmail.com>
2017-04-08 11:29:22 +0530

    modified mainactivity.java to display events.

--- app/src/main/java/com/example/adavi/academiccompanion/EditUserProfileActivity.java
+++ app/src/main/java/com/example/adavi/academiccompanion/EditUserProfileActivity.java
@@ -2,11 +2,9 @@ package com.example.adavi.academiccompanion;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditUserProfileActivity extends AppCompatActivity {
@@ -41,14 +39,6 @@ public class EditUserProfileActivity extends AppCompatActivity {
         if(flag == true)//insertion successful
         {
 
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
-            View header=navigationView.getHeaderView(0);
-
-            TextView uname = (TextView)header.findViewById(R.id.navbar_username_tv);
-            TextView uemail = (TextView)header.findViewById(R.id.navbar_useremail_tv);
-            uname.setText(myDB.getUserName());
-            uemail.setText(myDB.getUserEmail());
 
             Intent intent = new Intent(this,DisplayUserProfileActivity.class);
             startActivity(intent);
